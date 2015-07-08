e0c92a471a18f052ba0c6eba5f27b8b44779d5fe
jTinks <user@jTinks.fake>
2015-07-07 23:56:47 -0700

    also modified exceptions

--- exceptions.py
+++ exceptions.py
@@ -4,7 +4,8 @@
 print ""
 
 try:
-    print "any number divided by zero is", 7/0
+    print "Any number divided by zero is..."
+    print 7/0
 
 except ZeroDivisionError:
-    print "Not possible to divide by zero."
+    print "It's not possible to divide by zero."
